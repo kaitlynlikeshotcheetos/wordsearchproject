0831cf60b5885c27b01238d408c63e571c08cdc2
sk25860 <156994557+sk25860@users.noreply.github.com>
2024-03-11 12:57:05 -0700

    trying to connect for the 1st time

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,21 @@
 import express from 'express';
 import * as ctrl from '../controllers/mainController.js';
+import * as auth from '../controllers/authController.js';
 
 const router = express.Router();
 
-// Define routes
-router.get('/', ctrl.home);
+//login stuff 
+router.get('/login', auth.login);
+router.post('/login', auth.verifyLogin);
+router.get('/register', auth.register);
+router.post('/register', auth.verifyRegister);
+router.get('/logout', auth.logout);
+
+
+
+//pages 
+router.get('/', auth.isAuthenticated, ctrl.home);
+router.get('/login-page', ctrl.loginPage);
+
             
 export default router;
