82b1b452bb2b3159fa62cb69f62fd3cc8c7f3705
kaitlynlikeshotcheetos <157065037+kaitlynlikeshotcheetos@users.noreply.github.com>
2024-03-11 13:12:22 -0700

    update app.js

--- app.js
+++ app.js
@@ -1,30 +1,15 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import User from './models/TemplateModel.js';
-
-import dotenv from 'dotenv'; 
-dotenv.config({ path: 'process.env' });
-
+const express = require('express');
 const app = express();
+const routes = require('./routes/routes');
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
-  });
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+// Set view engine
 app.set('view engine', 'ejs');
 
-// Define routes
-import routes from './routes/routes.js'; 
+// Routes
 app.use('/', routes);
 
-const PORT = process.env.PORT || 3000; 
+// Start server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
